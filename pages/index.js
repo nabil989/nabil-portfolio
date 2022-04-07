a1eb40bb4a976da8ef26a8fd8385b5be244b2938
nabil989 <73457486+nabil989@users.noreply.github.com>
2022-04-07 14:04:57 -0500

    abt page updated

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </Head>
       <div className ='flex flex-col items-center justify-center mx-auto space-y-14 '>
-        <div className='justify-center text-center space-y-4 mx-auto '>
+        <div className='justify-center text-center space-y-6 mx-auto '>
           <Image src="/croppednabil.png" width={144} height={144}/>
           <h1 className='font-bold text-6xl '>Nabil Chowdhury</h1>
-          <p className='text-[#00000099] text-3xl font-medium'>Aspiring Software Engineer</p>
+          <p className='text-[#00000099] text-3xl font-medium pt-2'>Aspiring Software Engineer</p>
         </div>
         <div className=' flex flex-col space-y-12 mx-auto w-4/5 md:w-2/3 lg:w-1/2 justify-center align-middle'> 
           <p className='text-nowrap text-[#000000B3] text-center text-xl lg:text-base'>I am currently a Freshmen studying Computer Science at the University of Texas at Austin. I like to build apps in my free time just to mess around.</p>
@@ -29,7 +29,7 @@ export default function Home() {
           <Link href="mailto: nabilc377@utexas.edu" className=''>
               <div className=' flex flex-row duration-300 align-middle justify-center whitespace-nowrap text-center mx-auto w-1/3 sm:w-1/4 mt-12 border border-none rounded-lg bg-black py-4 text-white font-bold transform transition-all hover:scale-105'>
                 <div className = 'mr-2 animate-wave'>👋 </div>
-                <a className=''>Say hello</a>
+                <a className='mr-1 sm:mr-0'>Say hello</a>
               </div>
               
           </Link>

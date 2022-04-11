b3ec503da97eac0e04d47eb4c5834ea8347ba5b8
nabil989 <73457486+nabil989@users.noreply.github.com>
2022-04-11 15:30:16 -0500

    fixed grid on books page

--- pages/books.js
+++ pages/books.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 const Books = () => {
     return (
-        <div className='flex flex-col justify-center align-middle text-center'>
+        <div className='flex flex-col justify-center align-middle text-center items-center space-y-12'>
             <Head>
                 <title>Books</title>
                 <link rel="shortcut icon" href="/favicon.ico" />
@@ -11,12 +11,11 @@ const Books = () => {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
             </Head>
-            <div className='space-y-4 w-full '>
-                <h1 className='font-bold text-4xl'>Intriguing Books</h1>
-                <p className='text-[#000000B3]'>Just a collection of books that I've read that I personally find interesting. Click on the cover to get my take on the book.</p>
+            <div className='space-y-6 w-full flex flex-col justify-center align-middle items-center'>
+                <h1 className='font-bold text-5xl'>Intriguing Books</h1>
+                <p className='text-[#000000B3] text-lg w-full md:w-1/2 text-center'>Just a collection of books that I've read that I personally find to be interesting.</p>
             </div>
-            
-            <div className='grid  grid-cols-3 grid-rows-2 align-middle justify-center items-center w-1/2'>
+            <div className='hover:cursor-pointer grid gap-8 px-4 sm:px-0  sm:gap-16 grid-cols-3 grid-rows-2 align-middle justify-center items-center w-full sm:w-11/12 md:w-2/5 place-items-center'>
                 <div className='hover:scale-105 transition-all duration-300 '>
                     <Link href='/books/grit'>
                         <Image width='250px' height='250px' src='/../public/book_covers/grit.jpeg'/>
@@ -27,7 +26,8 @@ const Books = () => {
                         <Image width='250px' height='250px' src='/../public/book_covers/grit.jpeg'/>
                     </Link>
                 </div>
-                <div className='hover:scale-105 transition-all duration-300'>
+                <div className='hover:scale-105 transition-all duration-300 '>
+                    
                     <Link href='/books/grit'>
                         <Image width='250px' height='250px' src='/../public/book_covers/grit.jpeg'/>
                     </Link>

890a2e99d12dbd449434307ad7e37937cec283d1
nabil989 <73457486+nabil989@users.noreply.github.com>
2022-04-11 21:48:18 -0500

    fixed footer

--- components/layout.js
+++ components/layout.js
@@ -23,30 +23,15 @@ const Layout = ({ children }) => {
         <div>
             <Navbar toggle={toggle} isOpen={isOpen}/>
             <Dropdown isOpen={isOpen} toggle={toggle}/>
-            {/* <div className={`${isOpen ? 'overflow-hidden h-full' : 'hidden h-0'}`}>
-                <Footer/> 
-                <main className={`transition-all duration-500 ${isOpen ? ' opacity-0 h-0 transition-none ease-out ' : 'opacity-100'}`}>
-                    {children}
-                </main>
-            </div>
-            <div className={`${isOpen ? 'hidden h-0' : ''}`}>
-                <main className={`transition-all duration-500 ${isOpen ? ' opacity-0 h-0 transition-none ease-out ' : 'opacity-100'}`}>
-                    {children}
-                </main>
-                <Footer/> 
-
-            </div> */}
             <div className={`${!isOpen ? 'hidden h-0' : ''}`}>
-                <Footer /> 
+                <Footer toggle={toggle}/> 
             </div>
             <main className={`transition-all duration-500 ${isOpen ? 'overflow-hidden opacity-0 h-0 transition-none ease-out ' : 'opacity-100'}`}>
                 {children}
             </main>
             <div className={`${isOpen ? 'hidden h-0' : ''}`}>
-                <Footer /> 
+                <Footer toggle={toggle}/> 
             </div>
-            
-            {/* <Footer className={`${isOpen ? 'hidden': ''}`}/>  */}
         </div>
     );
 }
